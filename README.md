933e800bcf6a66d0fe340a28209bfe5f13f1e674
jumblesale <rob@robsollars.co.uk>
2015-09-14 14:50:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+symfony 2.7 skeleton app
+========================
+
+This is a really basic Symfony 2.7 app with none of the baggage that you get from running `symfony new`
+ 
+To get started just put your project name in `composer.json` and run composer install.
+ 
+The generator bundle is still in there so if you would like to start building a bundle, just run
+`app/console generate:bundle`
+
+Have fun and be good to each other.
+
+:eggplant:
---
+++ app/AppKernel.php
@@ -0,0 +1,28 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
